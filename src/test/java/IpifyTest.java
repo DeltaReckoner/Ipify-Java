679b9fdc14ee0bf94e35041669454e222198ab3b
DeltaReckoner <deltareckoner@gmail.com>
2024-12-30 16:56:24 -0500

    Add try-catch to test case

--- src/test/java/IpifyTest.java
+++ src/test/java/IpifyTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-
 import static ipify.models.IPFormat.IPFormatType.Text;
 
 public class IpifyTest {
@@ -17,11 +15,15 @@ public class IpifyTest {
     }
 
     @Test
-    void testGetIpAddress() throws ExecutionException, InterruptedException {
-        IPFormat ipFormat = new IPFormat.IPFormatBuilder(Text).build();
-        String ipAddress = ipifyClient.getIPAddress(ipFormat);
+    void testGetIpAddress() {
+        try {
+            IPFormat ipFormat = new IPFormat.IPFormatBuilder(Text).build();
+            String ipAddress = ipifyClient.getIPAddress(ipFormat);
 
-        Assertions.assertNotNull(ipAddress);
-        Assertions.assertFalse(ipAddress.isEmpty());
+            Assertions.assertNotNull(ipAddress);
+            Assertions.assertFalse(ipAddress.isEmpty());
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
     }
 }
